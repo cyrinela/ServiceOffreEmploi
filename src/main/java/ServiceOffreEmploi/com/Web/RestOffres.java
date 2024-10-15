3ed37afcb828d0dcbc756a0d77e796a955164d9e
cyrine laribi <cyrinelaribi1@gmail.com>
2024-10-15 20:11:21 +0100

    Initiation de mon projet serviceoffre

---
+++ src/main/java/ServiceOffreEmploi/com/Web/RestOffres.java
@@ -0,0 +1,36 @@
+package ServiceOffreEmploi.com.Web;
+
+import ServiceOffreEmploi.com.Entities.Offre;
+import ServiceOffreEmploi.com.Repositories.OffreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Offres")
+public class RestOffres {
+    @Autowired
+    OffreRepository offreRepository;
+    @GetMapping
+    public List<Offre> getAll() {
+        return offreRepository.findAll();
+    }
+
+    @GetMapping("/{uid}")
+    public Offre getbyid(@PathVariable Long uid ) {
+
+        return offreRepository.findById(uid).get();
+    }
+
+    @PostMapping
+    public Offre saveoffre(@RequestBody Offre newoffre) {
+        return offreRepository.save(newoffre);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteoffre(@PathVariable Long id) {
+        offreRepository.deleteById(id);
+    }
+
+}
---
+++ src/test/java/ServiceOffreEmploi/com/ServiceOffreEmploiApplicationTests.java
@@ -0,0 +1,13 @@
+package ServiceOffreEmploi.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOffreEmploiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
